b6e79ce11da6ab270e936208659c8befbf6d8ed2
Pedro Nieto <pedronieto.2005@gmail.com>
2026-04-29 22:09:45 -0300

    feat: update Puppeteer launch configuration to include additional arguments

--- src/services/mail/mail.service.ts
+++ src/services/mail/mail.service.ts
@@ -60,7 +60,15 @@ export class MailService {
 
       const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath:
+          this.configService.get<string>('PUPPETEER_EXECUTABLE_PATH') ||
+          undefined,
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--single-process',
+        ],
       });
 
       let pdfBuffer: Buffer;
